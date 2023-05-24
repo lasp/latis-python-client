947d0036842214857ec9753ec96a626cc7fa39df
pie-tras <alex.e.pietras@gmail.com>
2023-05-24 12:36:33 -0600

    propagate changes

--- src/latis-python-client/latis.py
+++ src/latis-python-client/latis.py
@@ -145,11 +145,7 @@ class Dataset:
     def buildQuery(self):
         self.query = self.latisInstance.baseUrl + self.name + '.csv?'
 
-        for i in range(len(self.projections)):
-            p = self.projections[i]
-            self.query = self.query + urllib.parse.quote(p)
-            if not i == len(self.projections) - 1:
-                self.query = self.query + ','
+        self.query += ','.join(urllib.parse.quote(p) for p in self.projections)
 
         for s in self.selections:
             self.query = self.query + '&' + urllib.parse.quote(s)
